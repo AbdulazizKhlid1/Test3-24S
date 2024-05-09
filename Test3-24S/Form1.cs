683ff511f83572813690cf12eedb0b8a560b78d4
AbdulazizKhlid1 <s44400133@students.tu.edu.as>
2024-05-09 08:44:49 +0300

    Abdulaziz

--- Test3-24S/Form1.cs
+++ Test3-24S/Form1.cs
@@ -2,9 +2,70 @@ namespace Test3_24S
 {
     public partial class Form1 : Form
     {
+        double count;
+
+        double sum;
+
         public Form1()
         {
             InitializeComponent();
+        }
+       
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+
+            sum += double.Parse(textBox1.Text);
+
+            textBox2.Text = Convert.ToString(sum);
+
+            textBox3.Text = Convert.ToString(count);
+
+            textBox4.Text = $"= {string.Format("{0:0.00}", sum/count)}"; ;
+
+
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count = 0;
+
+            sum = 0;
+
+            textBox2.Text = " ";
+
+            textBox3.Text = " ";
+
+            textBox4.Text = " ";
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
